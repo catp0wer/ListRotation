2a66c7b0b5712f8f5b1d1f781ccbcab6ea7b8737
catp0wer <catp0wer@mail.ru>
2017-12-27 00:30:38 +0200

    unfinished log attempt

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
-
+    //private static Logger LOGGER = Logger.getLogger("InfoLogging");
+    private final Logger LOG = LoggerFactory.getLogger(App.class);
     public static void main(String[] args){
+      //  LOGGER.setLevel(Level.FINE);
+      //  LOGGER.info("Logging an INFO-level message");
+        LOG.info("sad"); //non-static field cannot be referenced in a static context
         Rotation result = new Rotation();
         ArrayList<Integer> myList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
         System.out.println("Enter the number of rotations: ");
@@ -14,4 +20,16 @@ public class App {
         myList=result.Rotate(numberOfRotations,myList);
         System.out.println("My rotated list is: " + myList);
     }
+
+//    public void submain(){
+//        LOG.info("initializing");
+//        Rotation result = new Rotation();
+//        ArrayList<Integer> myList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+//        System.out.println("Enter the number of rotations: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int numberOfRotations = scanner.nextInt();
+//
+//        myList=result.Rotate(numberOfRotations,myList);
+//        System.out.println("My rotated list is: " + myList);
+//    }
 }
--- src/Rotation.java
+++ src/Rotation.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Rotation {
     private static final int FIRST = 0;
 
+    private static Logger LOGGER = Logger.getLogger("InfoLogging");
+    static{
+        LOGGER.setLevel(Level.FINE);
+    }
+   // LOGGER.setLevel(Level.FINE);
     public ArrayList<Integer> Rotate(int numberOfRotations, ArrayList<Integer> myList) {
         int count = 0;
         for (int i = 0; i < numberOfRotations; i++) {
             int temp = myList.get(FIRST);
             myList.add(temp);
             myList.remove(FIRST);
-            System.out.println(myList);
+
+            LOGGER.fine(myList.toString());
+           // System.out.println(myList);
 
         }return myList;
 
